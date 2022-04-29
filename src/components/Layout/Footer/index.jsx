4290b5164af9bdd3b842a7675fdb315fed8bad5d
Javi1499 <fruiz1@ucol.mx>
2022-04-29 14:55:03 -0500

    Footer information completed

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "./style.scss";
+import Moment from "react-moment";
+
 const Footer = () => {
+  const date = new Date();
+  const format = "DD/MM/YYYY";
   return (
     <div className="footer">
       <div className="footer__section">
-        <p className="footer__text">Date</p>
+        <p className="footer__text">
+          <Moment format={format}>{date}</Moment>
+        </p>
       </div>
       <div className="footer__section">
-        <p className="footer__text">Repository</p>
+        <a
+          target="_blank"
+          href="https://github.com/Javi1499/test-javier-ruiz"
+          className="footer__text"
+        >
+          App Repository
+        </a>
       </div>
       <div className="footer__section">
-        <p className="footer__text">My name</p>
+        <p className="footer__text">Created by Javier Ruiz</p>
       </div>
     </div>
   );
